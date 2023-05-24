94c5e43856b33662d5933c042a01fd209e869b07
hyojin park <candy020406@naver.com>
2023-05-24 17:51:36 +0900

    DayCalender

--- DayCalenderUI.cs
+++ DayCalenderUI.cs
@@ -19,27 +19,41 @@ namespace soft_team9
 
         private void LogOutbutton_Click(object sender, EventArgs e)
         {
-
+            // 로그아웃 하시겠습니까 ? 메시지창 띄운 다음 확인버튼 누르면 창 변경  
+            DialogResult result = MessageBox.Show("로그아웃하시겠습니까?", "Logout", MessageBoxButtons.OKCancel);
+            if(result == DialogResult.OK)
+            {
+                this.Close();
+                LoginUI loginUI = new LoginUI();
+                loginUI.Show();
+            }                               
         }
 
         private void ScheduleAddButton_Click(object sender, EventArgs e)
         {
-
+            // 현재 창은 닫고, 수정하는 창 띄어줌
+            this.Close();           
+            DetailedScheduleUI detailedScheduleUI = new DetailedScheduleUI();
+            detailedScheduleUI.Show();
         }
 
         private void ScheduleModifyButton_Click(object sender, EventArgs e)
         {
-
+            // 현재 창은 닫고, 수정하는 창 띄어줌
+            this.Close();          
+            DetailedScheduleUI detailedScheduleUI = new DetailedScheduleUI();
+            detailedScheduleUI.Show();
         }
 
         private void ScheduleDeleteButton_Click(object sender, EventArgs e)
         {
-
+            //해당 내용 삭세
+            //db에도 해당 내용 삭제
         }
 
         private void MonthReturnButton_Click(object sender, EventArgs e)
         {
-
+            this.Close(); //창 닫기(종료)
         }
     }
 }
