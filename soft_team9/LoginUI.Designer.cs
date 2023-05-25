4317f61595b368102da8bf7f4efdb3f70248c032
남정욱 <113816822+HelloWook@users.noreply.github.com>
2023-05-25 20:12:19 +0900

    1
    1

---
+++ soft_team9/LoginUI.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace soft_team9
+{
+    partial class LoginUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginUI));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserID_textBox = new System.Windows.Forms.TextBox();
+            this.UserPW_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.LogIn_button = new System.Windows.Forms.Button();
+            this.Cancel_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(57, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "아이디:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(57, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "비밀번호:";
+            // 
+            // UserID_textBox
+            // 
+            this.UserID_textBox.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.UserID_textBox.Location = new System.Drawing.Point(156, 110);
+            this.UserID_textBox.Name = "UserID_textBox";
+            this.UserID_textBox.Size = new System.Drawing.Size(248, 33);
+            this.UserID_textBox.TabIndex = 2;
+            // 
+            // UserPW_textBox
+            // 
+            this.UserPW_textBox.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.UserPW_textBox.Location = new System.Drawing.Point(156, 159);
+            this.UserPW_textBox.Name = "UserPW_textBox";
+            this.UserPW_textBox.Size = new System.Drawing.Size(248, 33);
+            this.UserPW_textBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(125, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "스케줄 관리 프로그램";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel2.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.Location = new System.Drawing.Point(12, 38);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(453, 12);
+            this.linkLabel2.TabIndex = 26;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "                                                                                 " +
+    "                               ";
+            // 
+            // LogIn_button
+            // 
+            this.LogIn_button.BackColor = System.Drawing.Color.White;
+            this.LogIn_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogIn_button.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LogIn_button.Location = new System.Drawing.Point(291, 246);
+            this.LogIn_button.Name = "LogIn_button";
+            this.LogIn_button.Size = new System.Drawing.Size(84, 34);
+            this.LogIn_button.TabIndex = 27;
+            this.LogIn_button.Text = "확인";
+            this.LogIn_button.UseVisualStyleBackColor = false;
+            // 
+            // Cancel_button
+            // 
+            this.Cancel_button.BackColor = System.Drawing.Color.White;
+            this.Cancel_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cancel_button.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Cancel_button.Location = new System.Drawing.Point(381, 246);
+            this.Cancel_button.Name = "Cancel_button";
+            this.Cancel_button.Size = new System.Drawing.Size(84, 34);
+            this.Cancel_button.TabIndex = 28;
+            this.Cancel_button.Text = "취소";
+            this.Cancel_button.UseVisualStyleBackColor = false;
+            // 
+            // LoginUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(479, 292);
+            this.Controls.Add(this.Cancel_button);
+            this.Controls.Add(this.LogIn_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.UserPW_textBox);
+            this.Controls.Add(this.UserID_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginUI";
+            this.Text = "LoginUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UserID_textBox;
+        private System.Windows.Forms.TextBox UserPW_textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button LogIn_button;
+        private System.Windows.Forms.Button Cancel_button;
+    }
+}
